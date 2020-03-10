be38aa434e105d785ea5890894c136d3f5833572
Maher Attouche <maherattouche7@gmail.com>
2020-03-10 17:12:55 +0100

    Code principal Main

--- src/main/java/uvsq/pglp_3_2/App.java
+++ src/main/java/uvsq/pglp_3_2/App.java
@@ -10,5 +10,14 @@ public class App
 {
     public static void main( String[] args )
     {
+        ArrayList<Employee> Travailleurs=new ArrayList<Employee>();
+        Travailleurs.add(new Vendeur(4,500));
+        Travailleurs.add(new Manager(3,40));
+        int Sum=0;
+        for(int a=0;a<Travailleurs.size();a++)
+        {
+        	Sum=Sum+Travailleurs.get(a).calculSalaire();
+        }
+        System.out.println(Sum);
     }
 }
