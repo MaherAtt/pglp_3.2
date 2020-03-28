c9c1d7cd8d110e94c29b0e87d8c315e0907a4039
Maher Attouche <maherattouche7@gmail.com>
2020-03-28 17:35:56 +0100

    Pom.xml

--- src/main/java/uvsq/pglp_3_2/App.java
+++ src/main/java/uvsq/pglp_3_2/App.java
@@ -6,18 +6,16 @@ import java.util.ArrayList;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        ArrayList<Employee> Travailleurs=new ArrayList<Employee>();
-        Travailleurs.add(new Vendeur(4,500));
-        Travailleurs.add(new Manager(3,40));
-        int Sum=0;
-        for(int a=0;a<Travailleurs.size();a++)
-        {
-        	Sum=Sum+Travailleurs.get(a).calculSalaire();
-        }
-        System.out.println(Sum);
+public class App {
+  public static void main(String[] args){
+    ArrayList<Employee> travailleurs = new ArrayList<Employee>();
+    travailleurs.add(new Vendeur(4,500));
+    travailleurs.add(new Manager(3,40));
+    int sum=0;
+    for (int a = 0;a < travailleurs.size();a++)
+      {
+       sum=sum+travailleurs.get(a).calculSalaire();
+      }
+        System.out.println(sum);
     }
 }
